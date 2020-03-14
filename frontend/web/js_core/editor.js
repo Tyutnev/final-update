a4d073fdfa626e9faaa79cc54aade3614f76b2c0
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-14 20:57:17 +0800

    Update

--- frontend/web/js_core/editor.js
+++ frontend/web/js_core/editor.js
@@ -126,7 +126,7 @@ const editableHandler = (event) => {
             target.style.width = `${width}px`;
             target.style.height = `${height}px`;
 
-            $(CURRENT_EDIT_ELEMENT).fitText(3);
+            $(CURRENT_EDIT_ELEMENT).fitText();
 
             // get drag event
             frame.translate = drag.beforeTranslate;
