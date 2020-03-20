b55ea9b6d1178df1da81dfb1c22c5537e98af9f6
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-20 17:53:45 +0800

    Update

--- frontend/web/js_core/editor.js
+++ frontend/web/js_core/editor.js
@@ -15,8 +15,22 @@ const CURRENT_EDIT_ELEMENT = `[${CURRENT_EDIT}="true"]`;
 
 const EMPTY = '';
 
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgb.length === 4) ? "#" +
+        ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+}
+
 const updatePalette = (hex) => {
+    if (hex.match('rgb')) {
+        hex = rgb2hex(hex);
+    }
+
     $('.pcr-result').val(hex);
+    pickr.setColor(hex);
 };
 
 const updateCurrentFont = () => {
