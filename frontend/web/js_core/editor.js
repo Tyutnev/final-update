77755191de34dd6cb2c161b1fc946165b4d52145
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-25 18:53:14 +0800

    Update

--- frontend/web/js_core/editor.js
+++ frontend/web/js_core/editor.js
@@ -128,7 +128,7 @@ const editableHandler = (event) => {
     console.log(target.prop('tagName'));
 
     if (!draggable) {
-        draggable = new Moveable($('.main-svg-container').get(0), {
+        draggable = new Moveable($('.main-svg').get(0), {
             draggable: true,
             throttleDrag: 0,
             resizable: true,
